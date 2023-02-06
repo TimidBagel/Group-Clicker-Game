cbbb278df6cd4e45c61e51d45ea7643e6027e0db
KitChaos <97907903+KitChaos-DreamscapeStudios@users.noreply.github.com>
2023-02-06 14:21:48 -0500

    Added basic player stats

---
+++ scripts.js
@@ -0,0 +1,9 @@
+class Player{
+    constructor(Food, Buildings, Modifiers, Upgrades, FoodCap){
+        this.Food = 0
+        this.Buildings = []
+        this.Modifiers = []
+        this.Upgrades = []
+        this.FoodCap = 20000 //Temp
+    }
+}
