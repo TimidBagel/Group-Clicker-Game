f72f8726d6872b171a5f8ce1eb7b727d23cc465e
Iain Broomell <slaprabbit805@gmail.com>
2023-02-07 11:49:35 -0500

    added framework for events

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
-class Player{
-    constructor(food, buildings, modifiers, upgrades, foodCap){
+class Player {
+    constructor(food, buildings, modifiers, upgrades, foodCap) {
         this.food = 100
         this.buildings = []
         this.upgrades = []
@@ -10,35 +10,52 @@ class Player{
     }
 }
 const mainPlayer = new Player();
-class Building{
-    constructor(production, cost, name, id){
+class Building {
+    constructor(production, cost, name, id) {
         this.production = production,
-        this.cost = cost,
-        this.name = name,
-        this.id = id
+            this.cost = cost,
+            this.name = name,
+            this.id = id
     }
 }
 
-class Upgrade{ // please add all necessary upgrade components, remove comment when complete
-    constructor(cost, name, id){
+class Upgrade { // please add all necessary upgrade components, remove comment when complete
+    constructor(cost, name, id) {
         this.cost = cost,
+            this.name = name,
+            this.id = id
+    }
+}
+
+class EventOption{    
+    constructor(type, foodDelta, inflationDelta, productionDelta){ // and whatever other impacts 
+        this.type = type, // string
+        this.foodDelta = foodDelta, // float
+        this.inflationDelta = inflationDelta, // float
+        this.productionDelta = productionDelta // float
+    }
+}
+
+class Event { // add all necessary event components
+    constructor(name, description, options) {
         this.name = name,
-        this.id = id
+        this.severity = description,
+        this.options = options // pass in a list of option names
     }
 }
 
 const allBuildings = []
-const kibbleSerf = new Building(1, 5, "Kibble Serf", "kibbleserf")  
+const kibbleSerf = new Building(1, 5, "Kibble Serf", "kibbleserf")
 const kibbleCircle = new Building(5, 25, "Kibble Summoning Circle", "kibblecircle")
 allBuildings.push(kibbleCircle)
 allBuildings.push(kibbleSerf)
-function Tick(){
+function Tick() {
     //Produce food from buildings
     for (let i = 0; i < mainPlayer.buildings.length; i++) {
         var curBuil = mainPlayer.buildings[i]
         mainPlayer.food += curBuil.production / 100
         //When we add modifers we can modify this number and stuff ~K
-        
+
     }
     document.getElementById("currentfood_counter").innerHTML = `You have ${Math.round((mainPlayer.food))} food`
     //Sets Shop text
@@ -47,43 +64,43 @@ function Tick(){
         var amtOfBuilding = 0
         for (let e = 0; e < mainPlayer.buildings.length; e++) {
             var curPlayerBuil = mainPlayer.buildings[e]
-            if(curBuil.name == curPlayerBuil.name){
+            if (curBuil.name == curPlayerBuil.name) {
                 amtOfBuilding += 1
             }
         } // counter should be on serperate element. CHANGE THIS LATER!! ~ Iain
         document.getElementById(`buy_${curBuil.id}_button`).innerHTML = `Buy ${curBuil.name}: ${(curBuil.cost).toPrecision(2)} Food<br>You have ${amtOfBuilding} ${curBuil.name}(s)`//I can figure out getting the number from the player later this is just the simples ~K
     }
-    
+
     window.setTimeout(Tick, 1)
 }
-function BuyBuilding(building){
-    if(building.cost > mainPlayer.food){
+function BuyBuilding(building) {
+    if (building.cost > mainPlayer.food) {
         alert(`Not enough food to purchase ${building.name}!`)
     }
-    else{
+    else {
         mainPlayer.food -= building.cost;
-        building.cost += (building.cost/100)*mainPlayer.inflation
+        building.cost += (building.cost / 100) * mainPlayer.inflation
         mainPlayer.buildings.push(building)
     }
 }
 
-function BuyUpgrade(upgrade){
-    if (upgrade.cost > mainPlayer.food){
+function BuyUpgrade(upgrade) {
+    if (upgrade.cost > mainPlayer.food) {
         alert(`Not enough food to purchase ${upgrade.name}!`)
     }
-    else{
+    else {
         mainPlayer.food -= upgrade.cost
-        upgrade.cost += (upgrade.cost/100) * mainPlayer.inflation
+        upgrade.cost += (upgrade.cost / 100) * mainPlayer.inflation
         mainPlayer.upgrades.push(upgrade)
     }
 }
 
 // takes an event, which has a list of options, chooses behaviour based on button id
-function EventOption(event, optionNum){
+function EventOption(event, optionNum) {
 
 }
 
 // onclick function for abominable dog
-function Click(){
+function Click() {
     mainPlayer.food += 1//This will be improved later
 }
