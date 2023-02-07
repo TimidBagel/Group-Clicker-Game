63fa1c2ab567b7c57dc37dd4b0553bc2677371f4
KitChaos <97907903+KitChaos-DreamscapeStudios@users.noreply.github.com>
2023-02-07 10:19:54 -0500

    Added buildings
    More will be added in the future, I havent gotten more than one building to work with the text setting.

--- scripts.js
+++ scripts.js
@@ -1,11 +1,61 @@
 class Player{
     constructor(Food, Buildings, Modifiers, Upgrades, FoodCap){
-        this.food = 0
+        this.food = 100
         this.buildings = []
         this.modifiers = []
         this.upgrades = []
         this.foodCap = 20000 //Temporary, this number will be balanced.
+        this.Inflation = 22
     }
 }
-
 const mainPlayer = new Player();
+class Building{
+    constructor(Production, Cost, Name, id){
+        this.Production = Production
+        this.Cost = Cost,
+        this.Name = Name
+        this.id = id
+    }
+}
+const allBuildings = []
+const kibbleSerf = new Building(1, 5, "Kibble Serf", "kibbleserf")  
+
+allBuildings.push(kibbleSerf)
+function Tick(){
+    //Produce food from buildings
+    for (let i = 0; i < mainPlayer.buildings.length; i++) {
+        var curBuil = mainPlayer.buildings[i]
+        mainPlayer.food += curBuil.Production / 100
+        //When we add modifers we can modify this number and stuff ~K
+        
+    }
+    document.getElementById("currentfood_counter").innerHTML = `You have ${Math.round((mainPlayer.food))} food`
+    //Sets Shop text
+    for (let i = 0; i < allBuildings.length; i++) {
+        var curBuil = allBuildings[i]
+        var amtOfBuilding = 0
+        for (let e = 0; e < mainPlayer.buildings.length; e++) {
+            var curPlayerBuil = mainPlayer.buildings[i]
+            if(curBuil.Name = curPlayerBuil.Name){
+                amtOfBuilding += 1
+            }
+        }
+        document.getElementById(`buy_${curBuil.id}_button`).innerHTML = `Buy ${curBuil.Name}: ${(curBuil.Cost).toPrecision(2)} Food<br>You have ${amtOfBuilding} ${curBuil.Name}s`//I can figure out getting the number from the player later this is just the simples ~K
+        
+    }
+    
+    window.setTimeout(Tick, 1)
+}
+function BuyBuilding(building){
+    if(building.Cost > mainPlayer.food){
+        alert(`Not enough food to purchase ${building.Name}!`)
+    }
+    else{
+        mainPlayer.food -= building.Cost;
+        building.Cost += (building.Cost/100)*mainPlayer.Inflation
+        mainPlayer.buildings.push(building)
+    }
+}
+function Click(){
+    mainPlayer.food += 1//This will be improved later
+}
