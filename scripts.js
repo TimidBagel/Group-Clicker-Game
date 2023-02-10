0338a4a89a838597eb7f8110b33c8152b30e7612
Iain Broomell <slaprabbit805@gmail.com>
2023-02-10 10:12:54 -0500

    added some pretty cool comments

--- scripts.js
+++ scripts.js
@@ -10,6 +10,8 @@ class Player {
         this.stability = 80
     }
 }
+const mainPlayer = new Player(); // instantiates player
+
 class Dog{
     constructor(){
         this.fullness = 0
@@ -18,19 +20,21 @@ class Dog{
         this.foodFlation = 15
     }
 }
-const mainDog = new Dog();
+const mainDog = new Dog(); // instantiates dog
+
+// depletes player's food, increments dog's fullness
 function FeedDog(){
     if(mainPlayer.food >= mainDog.feedAmount){
         mainPlayer.food -= mainDog.feedAmount
         mainDog.fullness += 1
-        mainDog.feedAmount += ((mainDog.feedAmount/100)*mainDog.foodFlation)
+        mainDog.feedAmount += ((mainDog.feedAmount/100)*mainDog.foodFlation) // inflates food required by dog
         
     }
     else{
         alert("Not Enough food to feed dog!")
     }
 }
-const mainPlayer = new Player();
+
 
 class Building {
     constructor(production, cost, name, description, id, count) {
@@ -43,7 +47,7 @@ class Building {
     }
 }
 
-class Upgrade { // please add all necessary upgrade components, remove comment when complete
+class Upgrade {
     constructor(cost, name, id, description, modifier, count) {
         this.cost = cost
         this.name = name
@@ -71,15 +75,24 @@ class Event {
 
     }
 }
-//AddPlayerEffects
+
+// reveals an event in the event space
 function SpawnEvent(Event) {
-    document.getElementById("event_title").innerHTML = Event.title
-    document.getElementById("event_description").innerHTML = Event.description
-    SetActive(document.getElementById("cell_event_log_events"), false)
+    document.getElementById("event_title").innerHTML = Event.title // sets element to the event title
+    document.getElementById("event_description").innerHTML = Event.description // sets element to event description
+
+    SetActive(document.getElementById("cell_event_log_events"), false) // reveals the event element
+
     var optionString = ""//For demonstration.
     for (let i = 0; i < Event.options.length; i++) {
-        var option = Event.options[i];                                 //This is the problem error see Event-Error for details
-        optionString += `<button class='cell_event_log_event_choice' onclick='AddPlayerEffects(${JSON.stringify(option.effects)})' onmouseover='SetActive(document.getElementById("event_tooltip_${i}"), false)' onmouseleave='Disable(document.getElementById("event_tooltip_${i}"))' onmouseup='Disable(document.getElementById("cell_event_log_events"))'>${option.name}</button>`
+        var option = Event.options[i];
+
+        optionString += `<button class='cell_event_log_event_choice' onclick='AddPlayerEffects(${JSON.stringify(option.effects)})' 
+        onmouseover='SetActive(document.getElementById("event_tooltip_${i}"), false)' 
+        onmouseleave='Disable(document.getElementById("event_tooltip_${i}"))' 
+        onmouseup='Disable(document.getElementById("cell_event_log_events"))'>${option.name}</button>`
+
+        // sets tooltip as option description
         document.getElementById(`event_tooltip_${i}`).innerHTML = option.description
 
         //console.log(JSON.stringify(option.effects))
@@ -118,70 +131,34 @@ function Disable(div) {
 const allBuildings = []
 const allUpgrades = []
 
-/*
-for (let i = 0; i < allUpgrades.length; i++) {
-        var curUpg = allUpgrades[i]
-        var haveUpgrade = 0
-        for (let e = 0; e < mainPlayer.upgrades.length; e++) {
-            var curPlayerUpg = mainPlayer.upgrades[e]
-            if (curUpg.name == curPlayerUpg.name) {
-                haveUpgrade += 1
-            }
-
-        }
-        document.getElementById(`buy_${curUpg.id}_button`).innerHTML = `Buy ${curUpg.name}: ${Math.round((curUpg.cost))} Food<br>This upgrade ${curUpg.desc} ${curUpg.clicks}.`
-    }
-    window.setTimeout(Tick, 2)
-
-    const foodClick = new Upgrade(100, "Food Click (working title)", "foodclick", "permanently increases food per click by", 1)
-
-    */
-
-
 // building template: const buildingName = new Building(production, cost, "building name", "this is the building description", "buildingId", count)
 
-const kibbleSerf = new Building(1, 5, "Kibble Serf", "A worker to harvest more kibble", "kibbleSerf", 0)
-const kibbleCircle = new Building(5, 25, "Kibble Summoning Circle", "An occult circle to summon kibble from the Otherworld", "kibbleCircle", 0)
+
 
 const investment = new Modifer("Production", "investment", 0.15, 300)
 const smallClickBoost = new Modifer("Click Power", "Small Click Power Boost", 0.45, -100)
-
 const blackMarketBoost = new Modifer("Production", "A deal from the black market", 0.55, 1200)
 const policeClot = new Modifer("Production", "Beuracracy", -0.05, 300)
 const BaseDecay = new Modifer("Decay Rate", "The base Decay rate", 1, -100)
+const dogAttack = new Modifer("Production", "dog invasion", -0.1, 420)
 mainPlayer.modifiers.push(BaseDecay)
 
 
 const clickUpgrade = new Upgrade(300, "Small Click Upgrade", "clickUpgrade", "A Small click upgrade", smallClickBoost, 0)
 
-
-const kibblePortal = new Building(20, 150, "Kibble Portal", "An eldritch portal that brings in food from kibbhell","kibblePortal", 0)
-
-const dogAttack = new Modifer("Production", "dog invasion", -0.1, 420)
-//const clickUpgrade = new Upgrade(300, "Small Click Upgrade", "smallclickupgrade", "A Small click upgrade", smallClickBoost)
-
-allBuildings.push(kibbleSerf)
-allBuildings.push(kibbleCircle)
-
-//mainPlayer.modifiers.push(smallClickBoost)
 allUpgrades.push(clickUpgrade)
-//mainPlayer.modifiers.push(investment)
+
 let currentTab = ""
 const InvestmentEvent = new Event("Investment offer", "Your efforts to feed the dog are getting noticed. A company has come forth to offer support.", [new EventButton("Request an investment", "Gain +15% Production for 5 Minutes", { modifiers: [investment] }), new EventButton("Request a donation", "Gain 1234 food", { food: 1234 })])
-
 const InflationEvent = new Event("Rising food prices", "Kibble prices are rising globally, in no small part caused by your efforts to eliminate the dog. This could make expansion difficult", [new EventButton("Consolidate food and hope for the worst!", "Gain 3500 food<br>Gain 20% inflation", {food:3500, inflation:20}), new EventButton("Nothing I can do...", "Gain 10% inflation", {inflation:10}), new EventButton("Use wealth of food to support the industry", "Lose 1500 food<br> Gain 5% inflation", {food:-1500, inflation:5})])
 const DoggistAttack = new Event("Doggist Attack!", "A group of violent radicals, known as the doggists, have attacked your facilities. Ranting about \"All Dogs must be preserved! No matter how world-threatening\" and \"Since when did we not accept demanded blood sacrifices? how far our society has fallen\" They have damaged your facilites. How shall you proceed?", [new EventButton("Find these terrorists!", "Lose 4000 food<br>Gain 10% Stability<br> Lose one random building",{food:-4000, stability:10, buildingsLost:1}), new EventButton("Do nothing", "Lose one building", {buildingsLost: 1})])
 const BlackMarketEvent = new Event("A Shady Offer", "You have been appreached by a representative of the black market. He proposes a tantalizing offer: a small payment in exchange for longtime services. Surely he means well....", [new EventButton("But of Course!", "Lose 15% Stability<br>Lose 500 food<br>Gain a 55% Production bonus for 20 minutes", {food:-500, stability:-15, modifiers:[blackMarketBoost]}), new EventButton("Politely Decline", "", {food:0}), new EventButton("Rat him out to the police!", "Gain 5% Stability<br>Lose 50 food<br>Lose 5% Production for 5 minutes", {food:-50, stability:5, modifiers:[policeClot]})])
-//SpawnEvent(BlackMarketEvent)
-//SpawnEvent(doggistAttack)
-
-//SpawnEvent(InvestmentEvent)
-
 const DogInvasionEvent = new Event("Dog Invasion", "A dog army has found your kibble buildings. They now are attacking your buildings.", [new EventButton("Ignore dog invasion", "-10% Production for 7 minutes",{ modifiers: [dogAttack] }), new EventButton("Defend kibble buildings", "Dogs take 5000 food from your kibble buildings", { food: -5000})])
 const RobberyEvent = new Event("Your are being robbed!", 'The robber has "kindly" requested for 3500 kibble.', [new EventButton("Fork over kibble.","gives 3500 kibble to the robber.", {food: -3500}), new EventButton("Fight the robber!", "(This is risky)", AddPlayerEffects({inflation: 10}))])
 
-
-
+const kibbleSerf = new Building(1, 5, "Kibble Serf", "A worker to harvest more kibble", "kibbleSerf", 0)
+const kibbleCircle = new Building(5, 25, "Kibble Summoning Circle", "An occult circle to summon kibble from the Otherworld", "kibbleCircle", 0)
+const kibblePortal = new Building(20, 150, "Kibble Portal", "An eldritch portal that brings in food from kibbhell","kibblePortal", 0)
 const kibbleHele = new Building(35, 150, "Kibble Wretch", "This terrifying beast roams the countryside, gathering kibble and returning it to its master.", "kibbleHele", 0)
 const kibbleSpire = new Building(120, 500, "Kibble Obelisk", "Rising high and mighty, occult rituals are carried out atop this place.", "kibbleSpire", 0)
 const kibbleShip = new Building(500, 2000, "Kibble Spice Trade", "A spice road of kibble for you to control.", "kibbleShip", 0)
@@ -190,6 +167,8 @@ const kibbleFound = new Building(350, 50000, "Kibble Rune Foundry", "Using unfat
 const kibbleSpace = new Building(950, 200000, "Kibble Altar", "Turns blood into kibble! (May violate OSHA)", "kibbleSpace", 0)
 const kibbleNano = new Building(5000, 400000, "Kibblethullu", "A terrifying creature from the depths, only bound by a weak seal hastily fabricated. Who cares about impending doom when you can have kibble?", "kibbleNano", 0)
 
+allBuildings.push(kibbleSerf)
+allBuildings.push(kibbleCircle)
 allBuildings.push(kibbleHele)
 allBuildings.push(kibblePortal)
 allBuildings.push(kibbleSpire)
@@ -319,6 +298,8 @@ function AddPlayerEffects({ inflation = 0, food = 0, foodCap = 0, modifiers = []
         mainPlayer.buildings.push(Buildings[i])
     }
 }
+
+// purchases a building, adds it to list of player-owned buildings
 function BuyBuilding(building) {
     if (building.cost > mainPlayer.food) {
         alert(`Not enough food to purchase ${building.name}!`)
@@ -334,6 +315,7 @@ function BuyBuilding(building) {
     }
 }
 
+// purchases an upgrade, adds the apropriate modifier to the player
 function BuyUpgrade(upgrade) {
     if (upgrade.cost > mainPlayer.food) {
         alert(`Not enough food to purchase ${upgrade.name}!`)
@@ -372,7 +354,7 @@ function GetModifier(type) {//Returns the modifier
     return Modifer
 }
 
-
+// behavior for when the main clickable object is clicked
 function Click() {
     mainPlayer.food += 1 + ((1* GetModifier("Click Power")))
     const button = document.querySelector("#cell_food_button_food");
